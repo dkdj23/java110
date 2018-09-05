3556a53ea2b20ad0bd4dd7d2e49e6333cdad0858
jhan <duke8612@nate.com>
2018-09-05 10:18:26 +0900

    메뉴기능 추가

--- java110-project/src/main/java/App.java
+++ java110-project/src/main/java/App.java
@@ -28,15 +28,66 @@ public class App {
             this.password = password;
         }
     }
+    
+   
+    
     static Member[] members = new Member[100];
     static int index = 0;
     static Scanner keyIn = new Scanner(System.in);
 
     public static void main(String[] args) {
-        inputMembers();
-        printMembers();
         
+        while(true) {
+            String menu = promptMenu();
+            
+            if("1".equals(menu)) {
+                processStudentMenu();
+            }else if("0".equals(menu)){
+                System.out.println("안녕히 가세요!");
+                break;
+            }
+        }
         keyIn.close();
+    }
+
+    private static void processStudentMenu() {
+        while(true) {
+            System.out.println("학생 관리> ");
+            String command = keyIn.nextLine();
+            if("list".equals(command)) {
+                printMembers();
+            }else if("add".equals(command)){
+                inputMembers();
+            }else if("quit".equals(command)){
+                break;
+            }else {
+                System.out.println("유효하지 않는 명령입니다.");
+            }
+        }
+    }
+
+    private static String promptMenu() {
+        System.out.println("[메뉴]");
+        System.out.println("1.학생 관리");
+        System.out.println("2.강사 관리");
+        System.out.println("3.매니저 관리");
+        System.out.println("0.종료");
+        
+        while(true) {
+            System.out.println("메뉴 번호>");
+            String menu = keyIn.nextLine();
+        
+            switch(menu)
+            {
+                case "0":
+                case "1":
+                case "2":
+                case "3":
+                    return menu;
+                default:
+                    System.out.println("메뉴 번호가 유효하지 않습니다");
+            }
+        }
     }
     
     static void printMembers(){
