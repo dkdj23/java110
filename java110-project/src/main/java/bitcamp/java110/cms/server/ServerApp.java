5bdafce66d22ec9ced4ba002e709bdf3d5a812a8
jhan <duke8612@nate.com>
2018-09-17 13:07:12 +0900

    멀티스레드 적용

--- java110-project/src/main/java/bitcamp/java110/cms/server/ServerApp.java
+++ java110-project/src/main/java/bitcamp/java110/cms/server/ServerApp.java
@@ -57,55 +57,12 @@ public class ServerApp {
         System.out.println("서버 실행 중...");
         
         while (true) {
-            try (
-                Socket socket = serverSocket.accept();
-                PrintWriter out = new PrintWriter(
-                                    new BufferedOutputStream(
-                                            socket.getOutputStream()));
-                BufferedReader in = new BufferedReader(
-                                   new InputStreamReader(
-                                      socket.getInputStream()));
-            ) {
-                System.out.println(in.readLine());
-                out.println("OK"); out.flush();
-                
-                while (true) {
-                    String requestLine = in.readLine();
-                    
-                    if (requestLine.equals("EXIT")) {
-                        out.println("goodbye");
-                        out.println();
-                        out.flush();
-                        System.exit(0);
-                    }
-                    
-                    // 요청 객체 준비
-                    Request request = new Request(requestLine);
-                    
-                    // 응답 객체 준비
-                    Response response = new Response(out);
-                    
-                    RequestMappingHandler mapping = 
-                            requestHandlerMap.getMapping(request.getAppPath());
-                    if (mapping == null) {
-                        System.out.println("해당 요청을 처리할 수 없습니다.");
-                        out.println();
-                        out.flush();
-                        continue;
-                    }
-                    
-                    try {
-                        // 요청 핸들로 호출
-                        mapping.getMethod().invoke(mapping.getInstance(), request, response);
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                        out.println("요청 처리 중에 오류가 발생했습니다.");
-                    }
-                    out.println();
-                    out.flush();
-                }
-            }
+            Socket socket = serverSocket.accept();
+            RequestWorker worker = new RequestWorker(socket);
+//            worker.start();
+            new Thread(worker).start();
         }
+        
     }
     
     public static void main(String[] args) {
@@ -116,7 +73,73 @@ public class ServerApp {
             System.out.println(e);
         }
     }
+    
+//    class RequestWorker extends Thread {
+      class RequestWorker implements Runnable{
+        Socket socket;
+        public RequestWorker(Socket socket) {
+            this.socket = socket;
+        }
+
+        @Override
+        public void run() {
+            // 이 메서드에  main 스레드에서 분리하여 독립적으로 수행할 코드를 둔다.
+            try (
+                    Socket socket = this.socket;
+                    PrintWriter out = new PrintWriter(
+                                        new BufferedOutputStream(
+                                                socket.getOutputStream()));
+                    BufferedReader in = new BufferedReader(
+                                       new InputStreamReader(
+                                          socket.getInputStream()));
+                ) {
+                    System.out.println(in.readLine());
+                    out.println("OK"); out.flush();
+                    
+                    while (true) {
+                        String requestLine = in.readLine();
+                        
+                        if (requestLine.equals("EXIT")) {
+                            out.println("goodbye");
+                            out.println();
+                            out.flush();
+//                            System.exit(0);
+                        }
+                        
+                        // 요청 객체 준비
+                        Request request = new Request(requestLine);
+                        
+                        // 응답 객체 준비
+                        Response response = new Response(out);
+                        
+                        RequestMappingHandler mapping = 
+                                requestHandlerMap.getMapping(request.getAppPath());
+                        if (mapping == null) {
+                            System.out.println("해당 요청을 처리할 수 없습니다.");
+                            out.println();
+                            out.flush();
+                            continue;
+                        }
+                        
+                        try {
+                            // 요청 핸들로 호출
+                            mapping.getMethod().invoke(mapping.getInstance(), request, response);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                            out.println("요청 처리 중에 오류가 발생했습니다.");
+                        }
+                        out.println();
+                        out.flush();
+                    }
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        
+    }
 }
+
+
 
 
 
