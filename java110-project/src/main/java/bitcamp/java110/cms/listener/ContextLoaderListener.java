446a6f26501acdab8a90e67733e47c2d2a03fd05
jhan <duke8612@nate.com>
2018-09-28 09:34:52 +0900

    DAO 준비작업에 리스너 적용

--- java110-project/src/main/java/bitcamp/java110/cms/listener/ContextLoaderListener.java
+++ java110-project/src/main/java/bitcamp/java110/cms/listener/ContextLoaderListener.java
@@ -1,24 +1,22 @@
-package bitcamp.java110.cms.servlet;
+package bitcamp.java110.cms.listener;
 
 import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
 
 import bitcamp.java110.cms.dao.impl.ManagerMysqlDao;
 import bitcamp.java110.cms.dao.impl.StudentMysqlDao;
 import bitcamp.java110.cms.dao.impl.TeacherMysqlDao;
 import bitcamp.java110.cms.util.DataSource;
 
-// 이 서블릿의 배치 정보는 web.xml 에 둔다.
-public class InitServlet extends HttpServlet {
-
-    private static final long serialVersionUID = 1L;
-
+//@WebListener
+public class ContextLoaderListener implements ServletContextListener {
     @Override
-    public void init() throws ServletException {
-        System.out.println("InitServlet.init() 실행!");
+    public void contextInitialized(ServletContextEvent sce) {
+        System.out.println("ContextLoaderListener.contextInitialized() 실행!");
         
-        ServletContext sc = this.getServletContext();
+        ServletContext sc = sce.getServletContext();
         
         try {
             // DAO가 사용할 DB 커넥션풀 객체 준비
@@ -44,14 +42,9 @@ public class InitServlet extends HttpServlet {
             sc.setAttribute("studentDao", studentDao);
             sc.setAttribute("teacherDao", teacherDao);
         } catch (Exception e) {
-            throw new ServletException(e);
+            e.printStackTrace();
         }
-        
     }
     
-    // service() 또는 doGet(), doPost() 등의 메서드를 구현하지 않나?
-    // => 구현하지 않는다. 이 서블릿은 클라이언트 요청을 처리하기 위해 만든 서블릿이 아니다.
-    // => 클라이언트 요청을 처리하는 다른 서블릿들이 사용할 공용 자원을 준비하는 일을 한다.
-    // => 클라이언트가 요청하지 않아도 이 서블릿 객체과 생성되고 init() 가 실행되어야 하기 때문에
-    // => loadOnStartup 배치 속성을 추가한다.
+    
 }
