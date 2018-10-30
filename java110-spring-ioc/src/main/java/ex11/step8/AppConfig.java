65936ba67271bd1da2acbc13792388a08e8211b9
jhan <duke8612@nate.com>
2018-10-30 11:47:44 +0900

    Spring AOP 사용법

---
+++ java110-spring-ioc/src/main/java/ex11/step8/AppConfig.java
@@ -0,0 +1,10 @@
+package ex11.step8;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@ComponentScan(basePackages="ex11.step8")
+@EnableAspectJAutoProxy
+public class AppConfig {
+// 객체 생성하는 특별한 경우 아니면 안에 내용 없음
+}
